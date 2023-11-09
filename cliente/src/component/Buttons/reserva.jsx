56cbd5ec358556614dfc14fa27ae801587a12c8a
Dommingo Gonzalez <djgonzalez@miuandes.cl>
2023-11-09 20:52:56 -0300

    detalles en reserva

--- cliente/src/component/Buttons/reserva.jsx
+++ cliente/src/component/Buttons/reserva.jsx
@@ -2,24 +2,38 @@ import React from 'react';
 
 const HandleReserva = () => {
   //poner logica de reserva
-  console.log("Buscar casilleros disponibles")
-  console.log("De los disponibles, tomar los con el tamaño adecuado")
-  console.log("Asignar el casillero si hay disponibles")
-  console.log("Retornar casillero asignado")
+  console.log("Buscar casilleros disponibles");
+  console.log("De los disponibles, tomar los con el tamaño adecuado");
+  console.log("Asignar el casillero si hay disponibles");
+  console.log("Retornar casillero asignado si esque hay");
+  console.log("Casillero encontrado");
 }
 
+const HandleMailOperador = () => {
+  //poner logica de
+  console.log("Mail enviado a OPERADOR");
+
+}
+
+const HandleMailCliente = () => {
+  //poner logica de
+  console.log("Mail enviado a CLIENTE");
+
+}
 const BotonReserva = () => {
   const manejarClick = () => {
     console.log('Reservando');
-    console.log("Buscando casillero...")
-    HandleReserva()
-    console.log("Casillero encontrado")
-    console.log("Reservando casillero...")
-    console.log("Reserva realizada")
-    console.log("Su casillero es el 1")
-    console.log("Mandando mensaje al operador y cliente...")
-    console.log("Listo")
-    console.log("\n")
+    console.log("Buscando casillero...");
+    HandleReserva();
+    console.log('Si existe casillero se reserva');
+    console.log("Reservando casillero...");
+    console.log("Reserva realizada");
+    console.log("Su casillero es el 1");
+    console.log("Mandando mensaje al operador y cliente...");
+    HandleMailCliente();
+    HandleMailOperador();
+    console.log("Listo");
+    console.log("\n");
   };
 
   return (

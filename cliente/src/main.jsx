c9d4988920af675aa49c0684aee369e20267c695
Dommingo Gonzalez <djgonzalez@miuandes.cl>
2023-11-20 18:31:00 -0300

    ola

--- cliente/src/main.jsx
+++ cliente/src/main.jsx
@@ -1,23 +1,31 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate, useNavigate } from 'react-router-dom';
 import App from './App.jsx';
 import Login from './component/Registration/Login.jsx';
 import Cliente from './component/Cliente/Cliente.jsx'; // Import the Cliente component
-import Operador from './component/Operador/Operador.jsx'
 import './index.css';
+
+const LogoutButton = ({ onLogout }) => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    onLogout();
+    navigate('/login');
+  };
+
+  return <button onClick={handleClick}>Logout</button>;
+};
 
 const Main = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userType, setUserType] = useState(''); // Add a state variable for user type
   const [userId, setUserId] = useState(null);  // Add a state variable for user ID
 
-
   const handleLogin = (type, userId) => {
     setIsLoggedIn(true);
     setUserType(type);
     setUserId(userId);  // Save the user ID
-};
+  };
 
   const handleLogout = () => {
     setIsLoggedIn(false);
@@ -27,19 +35,15 @@ const Main = () => {
   return (
     <React.StrictMode>
       <Router>
-        {isLoggedIn && <button onClick={handleLogout}>Logout</button>}
+        {isLoggedIn && <LogoutButton onLogout={handleLogout} />}
         <Routes>
           <Route path="/" element={<Navigate to="/login" />} /> {/* Redirect from root path to login path */}
           {isLoggedIn ? (
             <>
               {userType === 'cliente' ? (
-                <Route path="/cliente" element={<Cliente />} />
-              ) : (
-              userType === 'operador' ? (
-                <Route path="/operador" element={<Operador />} />
+                <Route path="/cliente" element={<Cliente userId={userId} />} />
               ) : (
                 <Route path="/app" element={<App />} />
-              )
               )}
             </>
           ) : (

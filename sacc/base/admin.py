33b833270c3a91fe139d2b7e4f365448cd82d5db
GonzaVice <givicente@miuandes.cl>
2023-11-20 15:49:39 -0300

    Django Admin to Models

--- sacc/base/admin.py
+++ sacc/base/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import User, Reservation, Station, Locker
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Reservation)
+admin.site.register(Station)
+admin.site.register(Locker)

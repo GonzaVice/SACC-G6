cf73b36531677681b50ab65df914f03417c98dce
Tomas Rawlins <tfrawlins@miaundes.cl>
2023-11-21 17:01:16 -0300

    User sin userType

--- sacc/base/models.py
+++ sacc/base/models.py
@@ -4,21 +4,11 @@ from django.db import models
 
 #Modelo usuario
 class User(models.Model):
-    CLIENTE = 'cliente'
-    OPERADOR = 'operador'
-    ADMIN = 'admin'
-
-    USER_TYPES = [
-        (CLIENTE, 'Cliente'),
-        (OPERADOR, 'Operador'),
-        (ADMIN, 'Admin'),
-    ]
 
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=128)  # Use a hash solution to store passwords
-    user_type = models.CharField(max_length=20, choices=USER_TYPES, default=CLIENTE)
 
     def __str__(self):
         return self.email

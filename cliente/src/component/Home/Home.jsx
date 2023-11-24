bef1e1fd675ba11cb85187c4d8a5624b321d5c49
Tomas Rawlins <tfrawlins@miaundes.cl>
2023-11-23 21:44:07 -0300

    Home

--- cliente/src/component/Home/Home.jsx
+++ cliente/src/component/Home/Home.jsx
@@ -1,9 +1,57 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+
+const getCsrfToken = () => {
+    return document.cookie.split('; ')
+        .find(row => row.startsWith('csrftoken='))
+        .split('=')[1];
+}
 
 const Home = () => {
+    const [stations, setStations] = useState([]);
+    const navigate = useNavigate();
 
+    useEffect(() => {
+        const fetchStations = async () => {
+            try {
+        
+                const csrfToken = getCsrfToken();
+                const headers = {
+                    'X-CSRFToken': csrfToken,
+                };
 
-    //Lista de estaciones con botones a cada una
+                const response = await axios.get('http://127.0.0.1:8000/base/get_stations_info/', {
+                    headers,
+                    withCredentials: true,
+                });
 
+                setStations(response.data.station_info);
+                console.log(response)
+            } catch (error) {
+                console.error('Error al obtener las estaciones:', error);
+            }
+        };
+
+        fetchStations();
+    }, []); 
+
+    const handleButtonClick = (station) => {
+        navigate('/DashboardActualPorEstacion', { state: { station_name: station.name, connection: station.conexion } });
+    };
+
+    return (
+        <div>
+            <h1>Estaciones:</h1>
+            {stations.map((station) => (
+                <button key={station.name} onClick={() => handleButtonClick(station)}>
+                    {station.name}
+                </button>
+            ))}
+        </div>
+    );
 };
 
 export default Home;
+

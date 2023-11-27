4ba7c2901f8131b647a9f3b7585f24c405b8c3a4
Tomas Rawlins <tfrawlins@miaundes.cl>
2023-11-27 18:07:02 -0300

    daschaddsa

--- cliente/src/component/Tablas/TablaEstacion.jsx
+++ cliente/src/component/Tablas/TablaEstacion.jsx
@@ -1,9 +1,9 @@
 import {React, useState} from 'react';
 
-const TablaEstacion = ({ data }) => {
-
+const TablaEstacion = ({ data }) => { // 
+  console.log("DATOS TABLA", data)
   const handleViewReserva = (station) => {
-    console.log(`Redirigiendo a Reserva ${station.address}`);
+    console.log(`Redirigiendo a Reserva ${station.id}`);
   };
 
   const dimensionesString = (height, width, length) => (
@@ -17,6 +17,9 @@ const TablaEstacion = ({ data }) => {
   if (data.length === 0) {
     return <p>No hay datos disponibles.</p>;
   }
+
+  console.log("DATA TABLA", data)
+
 
   return (
     <table style={{ width: '100%', borderCollapse: 'collapse' }}>
@@ -34,35 +37,35 @@ const TablaEstacion = ({ data }) => {
         </tr>
       </thead>
       <tbody>
-      {data.map((casillero) => (
-        <tr key={casillero.id}>
-            <td style={tableCellStyle}>{casillero.id}</td>
-            <td style={tableCellStyle}>{dimensionesString(casillero.height, casillero.width, casillero.length)}</td>
-            <td style={tableCellStyle}>{casillero.state}</td>
-            <td style={tableCellStyle}>casillero.ocupado</td>
-            <td style={tableCellStyle}>casillero.abierto</td>
+      {data.lockers.map((locker) => (
+        <tr key={locker.name}>
+            <td style={tableCellStyle}>{locker.name}</td>
+            <td style={tableCellStyle}>{dimensionesString(locker.height, locker.width, locker.length)}</td>
+            <td style={tableCellStyle}>{locker.state}</td>
+            <td style={tableCellStyle}>locker.ocupado</td>
+            <td style={tableCellStyle}>locker.abierto</td>
             <td style={tableCellStyle}>
-                {casillero.reservado ? (
-                    <div key={casillero.reservations[casillero.reservations.length - 1].horaReserva}>
-                    {casillero.reservations[casillero.reservations.length - 1].horaReserva}
+                {locker.reservado ? (
+                    <div key={locker.reservations[locker.reservations.length - 1].horaReserva}>
+                    {locker.reservations[locker.reservations.length - 1].horaReserva}
                     </div>
                 ) : (
                     'N/A'
                 )}
             </td>
             <td style={tableCellStyle}>
-                {casillero.reservado ? (
-                    <div key={casillero.reservations[casillero.reservations.length - 1].horaCargado}>
-                    {casillero.reservations[casillero.reservations.length - 1].horaCargado}
+                {locker.reservado ? (
+                    <div key={locker.reservations[locker.reservations.length - 1].horaCargado}>
+                    {locker.reservations[locker.reservations.length - 1].horaCargado}
                     </div>
                 ) : (
                     'N/A'
                 )}
             </td>
-            <td style={tableCellStyle}>{casillero.porcentajeUso}</td>
+            <td style={tableCellStyle}>{locker.porcentajeUso}</td>
             <td style={tableCellStyle}>
-            {casillero.state!="Disponible" ? (
-            <button onClick={() => handleViewReserva(casillero.idReserva)}>
+            {locker.state!="Disponible" ? (
+            <button onClick={() => handleViewReserva(1)}>
                 Ver Reserva
             </button> ) : ('N/A')}
             </td>

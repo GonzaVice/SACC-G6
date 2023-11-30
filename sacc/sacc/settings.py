f92d9def17438674fdc80e7e3a277cd962b14b78
José Tomás Queirolo <jtqueirolo@miuandes.cl>
2023-11-29 21:42:12 -0300

    ADASDSADu

--- sacc/sacc/settings.py
+++ sacc/sacc/settings.py
@@ -5,6 +5,10 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SESSIONS_COOKIES_SECURE=True
+CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
